f777337f640a3c32def7465897563312736d867f
mynameisabdul <amalik11@uic.edu>
2017-10-06 00:09:59 -0500

    Update MoveHistory.java

--- MoveHistory.java
+++ MoveHistory.java
@@ -8,7 +8,7 @@ import java.util.*;
 
 //This class is for global variables
 public class MoveHistory {
-    public static Stack <String[]> stack = new Stack <>();
+    public static Stack <String[]> stack = new Stack <String[]>();
     public static int moveNum = 0;
     public static int complexity = 0;
 }
